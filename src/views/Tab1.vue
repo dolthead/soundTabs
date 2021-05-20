7a38a0e2a018b6faf5d95e2d48c9c0e46e1674c4
T Hale <tdhale@verisk.com>
2021-05-20 15:53:28 -0600

    setup composition for vue3

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -11,18 +11,37 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <ion-button expand="block" @click="play('sound1')">Play 1</ion-button>
+      <ion-button expand="block" @click="play('sound2')">Play 2</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+} from "@ionic/vue";
 
-export default  {
-  name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+export default {
+  name: "Tab1",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonButton,
+  },
+  setup(props: object) {
+    const play = (soundFile: string) => {
+      console.log({ soundFile, props });
+    };
+    return { play };
+  },
+};
 </script>
