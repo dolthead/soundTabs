49983505f574f4756cae18b07d302c8cbbe8ffd5
Todd H <github@simplybest.com>
2021-05-22 09:21:38 -0600

    add tab router navigation

--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -12,17 +12,25 @@
         </ion-toolbar>
       </ion-header>
       
-      <ExploreContainer name="Tab 3 page" />
+      <ion-button expand="block" @click="goToTab1()">Go to tab 1</ion-button>
+      
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+import { useRouter } from 'vue-router';
 
 export default  {
   name: 'Tab3',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton },
+  setup() {
+    const router = useRouter();
+    const goToTab1 = () => {
+      router.replace('/tabs/tab1');
+    };
+    return { goToTab1 };
+  }
 }
 </script>
